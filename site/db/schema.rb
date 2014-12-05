86d00d04f90197e7b34efa3414d70c32c9ab93e1
Jacob Sievers <jacosiev@gmail.com>
2014-12-05 10:38:07 +0100

    Switched to using postgres instead of sqlite

--- site/db/schema.rb
+++ site/db/schema.rb
@@ -13,6 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20141127135956) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "hstore"
+
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "text"
@@ -20,16 +24,6 @@ ActiveRecord::Schema.define(version: 20141127135956) do
     t.datetime "updated_at"
     t.string   "author"
   end
-
-  create_table "user_sessions", force: true do |t|
-    t.string   "session_id", null: false
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "user_sessions", ["session_id"], name: "index_user_sessions_on_session_id"
-  add_index "user_sessions", ["updated_at"], name: "index_user_sessions_on_updated_at"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -46,7 +40,7 @@ ActiveRecord::Schema.define(version: 20141127135956) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
